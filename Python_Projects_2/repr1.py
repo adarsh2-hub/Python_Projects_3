4630e48df84937cdcf308565054b9f783f7deffb
Adarsh Gouda <goudaadu146@gmail.com>
2026-09-23 19:36:01 +0530

    represent string problem

---
+++ Python_Projects_2/repr1.py
@@ -0,0 +1,9 @@
+class Book:
+    def __init__(self,title,author,year):
+        self.title=title
+        self.author=author
+        self.year=year
+    def __repr__(self):
+        return f"Book(title='{self.title}',author='{self.author}',year='{self.year}')"
+book=Book("Python Basics","John",2025)
+print(book)
